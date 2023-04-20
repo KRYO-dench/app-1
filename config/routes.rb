a8b5493e6d24ae300fe53de5002be51e0e148757
Alexander <kryvondenchenkov@gmail.com>
2023-04-20 22:19:15 +0300

    intermediate result

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do  
-root 'posts#index'
-get 'about' => 'pages#about'
+root 'posts#index', as: 'home'
+get 'about' => 'pages#about', as: 'about'
+resources :posts do
+    resources :comments
+end
 end
